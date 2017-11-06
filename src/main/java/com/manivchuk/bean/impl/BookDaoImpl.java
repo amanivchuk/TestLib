f3ec8e8acf18b1083985a5b7f44875cffee432c0
nec <amanivchuk@gmail.com>
2017-11-06 12:01:34 +0200

    commit

--- src/main/java/com/manivchuk/bean/impl/BookDaoImpl.java
+++ src/main/java/com/manivchuk/bean/impl/BookDaoImpl.java
@@ -6,10 +6,7 @@ import com.manivchuk.entities.Book;
 import com.manivchuk.entities.Genre;
 import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.DetachedCriteria;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.ProjectionList;
-import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.*;
 import org.hibernate.transform.Transformers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -49,9 +46,12 @@ public class BookDaoImpl implements BookDao {
                 .createCriteria(Book.class)
                 .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
 */
-        DetachedCriteria bookListCriteria = DetachedCriteria.forClass(Book.class, "b");
+ /*     DetachedCriteria bookListCriteria = DetachedCriteria.forClass(Book.class, "b");
         createAliases(bookListCriteria);
         List<Book> books = creteBookList(bookListCriteria);
+        return books;*/
+
+        List<Book> books = creteBookList(createBookCriteria());
         return books;
     }
 
@@ -65,20 +65,30 @@ public class BookDaoImpl implements BookDao {
         criteria.addOrder(Order.asc("b.name")).setProjection(projectionList).setResultTransformer(Transformers.aliasToBean(Book.class));
         return criteria.list();
     }
+    private DetachedCriteria createBookCriteria(){
+        DetachedCriteria bookListCriteria = DetachedCriteria.forClass(Book.class, "b");
+        createAliases(bookListCriteria);
+        return bookListCriteria;
+    }
 
+    @Transactional
     public List<Book> getBooks(Author author) {
-        return null;
+        List<Book> books = creteBookList(createBookCriteria().add(Restrictions.ilike("author.fio", author.getFio(), MatchMode.ANYWHERE)));
+        return books;
     }
-
+    @Transactional
     public List<Book> getBooks(String bookName) {
-        return null;
+        List<Book> books = creteBookList(createBookCriteria().add(Restrictions.ilike("b.name", bookName, MatchMode.ANYWHERE)));
+        return books;
     }
-
+    @Transactional
     public List<Book> getBooks(Genre genre) {
-        return null;
+        List<Book> books = creteBookList(createBookCriteria().add(Restrictions.ilike("author.fio", genre.getName(), MatchMode.ANYWHERE)));
+        return books;
     }
-
+    @Transactional
     public List<Book> getBooks(Character letter) {
-        return null;
+        List<Book> books = creteBookList(createBookCriteria().add(Restrictions.ilike("b.name", letter.toString(), MatchMode.START)));
+        return books;
     }
 }
